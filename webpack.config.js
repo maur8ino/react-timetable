6abb42bf8cd9c705e5809f0fe374ebacf578ac6b
Mauro Verrocchio <mauro@gild.com>
2015-03-21 19:03:18 +0100

    config file setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './public/index'
+  ],
+  output: {
+    path: __dirname + '/public/',
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['react-hot', 'babel'],
+      exclude: /node_modules/
+    }]
+  }
+};
