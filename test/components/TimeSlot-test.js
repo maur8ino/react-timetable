bc01f4c0a7c0f7461ea72f61af0c62957b98a2cb
Mauro Verrocchio <mauroverrocchio@gmail.com>
2016-03-20 20:52:13 +0100

    switches from mocha+chai to ava+unexpected

--- test/components/TimeSlot-test.js
+++ test/components/TimeSlot-test.js
@@ -1,33 +1,27 @@
+import test from 'ava';
 import React from 'react';
 import { createRenderer } from 'react-addons-test-utils';
-import { expect } from 'chai';
 import moment from 'moment';
 
 import TimeSlot from '../../src/components/TimeSlot.jsx';
 
-describe('TimeSlot', () => {
-  const renderer = createRenderer();
+const renderer = createRenderer();
+const now = moment();
+const duration = moment.duration(15, 'minutes');
+const from = 'From: '+ now.format(moment.defaultFormat);
+const to = 'To: ' + moment(now).add(duration).format(moment.defaultFormat);
+// Render a timeslot using shallow renderer
+renderer.render(<TimeSlot startTime={now} duration={duration} />);
+const timeslot = renderer.getRenderOutput();
 
-  describe('rendered with \'from\' and \'to\'', () => {
-    let now = moment();
-    let duration = moment.duration(15, 'minutes');
-    let from = 'From: '+ now.format(moment.defaultFormat);
-    let to = 'To: ' + moment(now).add(duration).format(moment.defaultFormat);
+test('<TimeSlot> should rendered in a div', (t) => {
+  t.is(timeslot.type, 'div');
+});
 
-    // Render a timeslot using shallow renderer
-    renderer.render(<TimeSlot startTime={now} duration={duration} />);
-    let timeslot = renderer.getRenderOutput();
+test('<TimeSlot> should have the right text in \'from\'', (t) => {
+  t.is(timeslot.props.children[0].props.children.join(''), from);
+});
 
-    it('should rendered in a div', () => {
-      expect(timeslot.type).to.equal('div');
-    });
-
-    it('should have the right text in \'from\'', () => {
-      expect(timeslot.props.children[0].props.children.join('')).to.equal(from);
-    });
-
-    it('should have the right text in \'to\'', () => {
-      expect(timeslot.props.children[1].props.children.join('')).to.equal(to);
-    });
-  });
+test('<TimeSlot> should have the right text in \'to\'', (t) => {
+  t.is(timeslot.props.children[1].props.children.join(''), to);
 });
